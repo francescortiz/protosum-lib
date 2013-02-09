2665f4016241ff0f6b0a1ca59e5563c22e492e24
franki <francescortiz@gmail.com>
2013-02-09 11:40:45 +0100

    improve clearEventListeners function improve jsdoc rename Event and EventDispatcher to PCEvent and PCEventDispatcher some general refactoring

--- pacaclass.event.js
+++ pacaclass.event.js
@@ -8,10 +8,10 @@ LIMITATIONS:
   - There is no priority (yet). New event bindings get execute last.
 
 USAGE:
-  - new EventDispatcher(); // Creates and event dispatcher instance
-  - var CustomEvent = PacaClass(Event); // Extends class Event to create a custom event.
+  - new PCEventDispatcher(); // Creates and event dispatcher instance
+  - var CustomEvent = PacaClass('CustomEvent', PCEvent); // Extends class PCEvent to create a custom event.
 
-  EventDispatcher methods:
+ PCEventDispatcher methods:
     - bind(eventClass, handlerFunction, scope, [lowPriority:boolean])
         * by default new event listeners get called first in order to have event.preventDefault() useful.
     - unbind(eventClass, [handlerFunction], [scope])
@@ -19,7 +19,7 @@ USAGE:
         * it removes all events that match the given parameters.
     - trigger(eventInstance)
 
-  Event methos:
+ PCEvent methos:
     - preventDefault()
         * stops event queue execution after this handlerFunction finishes.
 
@@ -27,7 +27,7 @@ USAGE:
 
 
 EXAMPLE:
-    var CustomEvent = PacaClass(Event); (function() {var public = CustomEvent.prototype;
+    var CustomEvent = PacaClass('CustomEvent', PCEvent); (function() {var public = CustomEvent.prototype;
 
         public.name = "customevent";
 
@@ -47,7 +47,7 @@ EXAMPLE:
 
     })();
 
-    var ed = new EventDispatcher();
+    var ed = new PCEventDispatcher();
 
     ed.bind(CustomEvent, c.customeEventHandler, c);
     ed.trigger(new CustomEvent("custom event data")); // C.customeEventHandler gets called
@@ -64,31 +64,38 @@ CONSIDERATIONS:
  *
  * @class
  */
-window.Event = PacaClass('Event');  (function() {var public = Event.prototype;
+window.PCEvent = PacaClass('PCEvent');  (function() {var public = PCEvent.prototype;
 
-    /** @lends Event */
+    /** @lends PCEvent */
 
     /**
-     * @constructor
-     * @param eventName
+     * if set to true, no more events will be executed after this one.
+     * @type Boolean
      */
-    public.constructor = function(eventName) {
-        this.name = eventName;
+    public.default_prevented = false;
+
+    /**
+     * Creates an event
+     * @param name  String  name of the event to create.
+     * @constructor
+     */
+    public.constructor = function(name) {
+        this.name = name;
     }
 
     /**
-     *
+     * If called, no more events are executed after this event
      */
     public.preventDefault = function() {
-        this.defaultPrevented = true;
+        this.default_prevented = true;
     }
 
     /**
-     *
-     * @return {Boolean}
+     * Checks where we prevented default
+     * @return Boolean
      */
     public.isDefaultPrevented = function() {
-        return this.defaultPrevented;
+        return this.default_prevented;
     }
 
 })();
@@ -98,57 +105,61 @@ window.Event = PacaClass('Event');  (function() {var public = Event.prototype;
  *
  * @class
  */
-window.EventDispatcher = PacaClass('EventDispatcher');  (function() {var public = EventDispatcher.prototype;
-
-    public.registeredEvents = {};
+window.PCEventDispatcher = PacaClass('PCEventDispatcher');  (function() {var public = PCEventDispatcher.prototype;
 
     /**
-     * @constructs EventDispatcher
+     * Contains the list of all registered events in the application.
+     * @type {Object}
+     */
+    public.registered_events = {};
+
+    /**
+     * @constructs PCEventDispatcher
      */
     public.constructor = function() {
 
     }
 
     /**
-     *
-     * @param eventName {String}
-     * @param handler {Function}
-     * @param handlerScope {Object}
+     * Adds an event listener
+     * @param name            String    name of the event
+     * @param handler         Function  function to execute when the event is triggered
+     * @param scope           Object    object to set the scope of the event to
+     * @param [low_priority]  Boolean   if true, the event will be pushed to the end of the event queue
      */
-    public.addEventListener = function(eventName, handler, handlerScope) {
+    public.addEventListener = function(name, handler, scope, low_priority) {
         if (!handler) {
-            throw new Error("EventDispatcher.addEventListener: Undefined handler provided for " + eventName + " on " + handlerScope.__class__.__name__);
+            throw new Error("PCEventDispatcher.addEventListener: Undefined handler provided for " + name + " on " + scope.__class__.__name__);
         }
-        if (!handlerScope) {
-            throw new Error("EventDispatcher.addEventListener: Undefined handlerScope provided for " + eventName + ".");
+        if (!scope) {
+            throw new Error("PCEventDispatcher.addEventListener: Undefined scope provided for " + name + ".");
         }
-        var lowPriority = arguments[3];
-        var handlerList = this.registeredEvents[eventName];
-        if (!handlerList) {
-            handlerList = [];
+        var handler_list = this.registered_events[name];
+        if (!handler_list) {
+            handler_list = [];
         }
-        if (lowPriority) {
-            handlerList.push([handler, handlerScope]);
+        if (low_priority) {
+            handler_list.push([handler, scope]);
         } else {
-            handlerList.unshift([handler, handlerScope]);
+            handler_list.unshift([handler, scope]);
         }
-        this.registeredEvents[eventName] = handlerList;
+        this.registered_events[name] = handler_list;
 
         // Be friendly with garbage collector TODO: UNTESTED!!!
-        if (!handlerScope.__event_dispatchers__) {
-            handlerScope.__event_dispatchers__ = [];
+        if (!scope.__event_dispatchers__) {
+            scope.__event_dispatchers__ = [];
         }
-        var len = handlerScope.__event_dispatchers__.length;
+        var len = scope.__event_dispatchers__.length;
         var found = false;
         for (var i = 0; i < len; i++) {
-            if (handlerScope.__event_dispatchers__[i] == this) {
+            if (scope.__event_dispatchers__[i] == this) {
                 found = true;
             }
         }
-        if (!handlerScope.__original_destroy__) {
-            handlerScope.__event_dispatchers__.push(this);
-            handlerScope.__original_destroy__ = handlerScope.destroy;
-            handlerScope.destroy = function() {
+        if (!scope.__original_destroy__) {
+            scope.__event_dispatchers__.push(this);
+            scope.__original_destroy__ = scope.destroy;
+            scope.destroy = function() {
                 this.__original_destroy__.apply(this, arguments);
                 var len = this.__event_dispatchers__.length;
                 for (var i = 0; i < len; i++) {
@@ -158,90 +169,94 @@ window.EventDispatcher = PacaClass('EventDispatcher');  (function() {var public 
         }
     };
 
-    /***************************
-    trigger(event)
-    ****************************/
+    /**
+     * Triggers an event
+     * @param event  PCEvent  event to dispatch
+     */
     public.dispatchEvent = function(event) {
-        if (!event.isInstance(Event)) {
-            throw new Error("EventDispatcher.trigger: event is not Event instance. Event = " + event);
+        if (!event || !event.isInstance || !event.isInstance(PCEvent)) {
+            throw new Error("PCEventDispatcher.trigger: event is not PCEvent instance. PCEvent = " + event);
         }
-        var handlerList = this.registeredEvents[event.name];
-        if (handlerList) {
-            var len = handlerList.length;
+        var handler_list = this.registered_events[event.name];
+        if (handler_list) {
+            var len = handler_list.length;
             for (var i = 0; i < len; i++) {
-                var handlerData = handlerList[i];
-                var func = handlerData[0];
-                var scope = handlerData[1];
+                var handler_data = handler_list[i];
+                var func = handler_data[0];
+                var scope = handler_data[1];
                 func.call(scope, event);
-                if (event.defaultPrevented) {
+                if (event.default_prevented) {
                     return;
                 }
             }
         }
     };
 
-    /***************************
-    unbind(event, [handler], [scope])
-    ****************************/
-    public.removeEventListener = function(eventName) {
-        var eventName = eventName;
-        var handler = arguments[1];
-        var handlerScope = arguments[2];
-
-        if (!handler && !handlerScope) {
-            this.registeredEvents[eventName] = {};
+    /**
+     * Removes and event listener
+     * @param name       String    name of the event
+     * @param [handler]  Function  function associated to the event. If omited al handlers are removed
+     * @param [scope]    Object    object to remove event from. If omited all objects are removed.
+     */
+    public.removeEventListener = function(name, handler, scope) {
+        if (!handler && !scope) {
+            this.registered_events[name] = {};
         }
 
-        var handlerList = this.registeredEvents[eventName];
-        if (handlerList) {
-            var len = handlerList.length;
+        var handler_list = this.registered_events[name];
+        if (handler_list) {
+            var len = handler_list.length;
             var nhd = [];
             for (var i = 0; i < len; i++) {
-                var handlerData = handlerList[i];
-                var func = handlerData[0];
-                var scope = handlerData[1];
+                var handler_data = handler_list[i];
+                var func = handler_data[0];
+                var scope = handler_data[1];
                 if (!handler) {
                     // Si no pasamos handler queremos vaciar todas las referencias del evento para este objeto.
-                    if (handlerScope == scope) {
+                    if (scope == scope) {
                         continue;
                     }
                 } else {
-                    if (!handlerScope || handlerScope == scope) {
+                    if (!scope || scope == scope) {
                         // Si no pasamos scope, borramos todas las referencias de handler, sinó, solo las del scope especificado.
                         continue;
                     }
                 }
-                nhd.push(handlerData);
+                nhd.push(handler_data);
             }
-            this.registeredEvents[eventName] = nhd;
+            this.registered_events[name] = nhd;
 
         }
 
     };
 
     /**
-     *
-     * @param [handlerScope] if specified, remove only events associated with this target.
+     * Removes all event listeners.
+     * @param [scope]    Object    if specified, remove only events associated with this scope
+     * @param [handler]  Function  if specified, remove only events associated with this handler function
      */
-    public.clearEventListeners = function(handlerScope) {
-        // TODO: UNTESTED!!!!
-        if (!handlerScope) {
-            this.registeredEvents = {};
+    public.clearEventListeners = function(scope, handler) {
+        if (!scope && !handler) {
+            this.registered_events = {};
         } else {
-            for (var eventName in this.registeredEvents) {
-                var handlerList = this.registeredEvents[eventName];
-                var len = handlerList.length;
+            for (var event_name in this.registered_events) {
+                var handler_list = this.registered_events[event_name];
+                var len = handler_list.length;
                 var nhd = [];
                 for (var i = 0; i < len; i++) {
-                    var handlerData = handlerList[i];
-                    var func = handlerData[0];
-                    var scope = handlerData[1];
-                    if (handlerScope == scope) {
+                    var handler_data = handler_list[i];
+                    var _handler = handler_data[0];
+                    var _scope = handler_data[1];
+                    if (_scope == scope && !handler) {
+                        continue;
+                    } else if (!scope && _handler == handler) {
+                        continue;
+                    } else if (_scope == scope && _handler == handler) {
                         continue;
                     }
-                    nhd.push(handlerData);
+                    nhd.push(handler_data);
                 }
-                this.registeredEvents[eventName] = nhd;
+                this.registered_events[event_name] = nhd;
             }
         }
     }
